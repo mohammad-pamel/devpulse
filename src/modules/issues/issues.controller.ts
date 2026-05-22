454b9f4acb0048c4752315a504034a0c0095cd1f
Pamel-C223171 <Mohammadpamel12345@gmail.com>
2026-05-22 07:28:40 +0300

    create to the issues and use to the moduler pattern insert the data inn table

---
+++ src/modules/issues/issues.controller.ts
@@ -0,0 +1,40 @@
+import type { Request, Response } from "express";
+import sendResponse from "../../utility/sendResponse";
+import { issuesService } from "./issues.service";
+
+const createIssues = async (req: Request, res: Response) => {
+    console.log("issues controller", req.body);
+    // const { name, email } = req.body;
+
+    try {
+        console.log("controller hit", req.body)
+
+        const payload = {
+            ...req.body,
+            reporter_id: req.user?.id,
+        };
+
+        const result = await issuesService.issuesCreateIntoDB(payload);
+
+        sendResponse(res, {
+            statusCode: 201,
+            success: true,
+            message: "User Created Successfully",
+            data: result.rows[0],
+        })
+
+    } catch (error: any) {
+        sendResponse(res, {
+            statusCode: 500,
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
+}
+
+
+export const issuesController = {
+    createIssues,
+    getAllIssues
+}
---
+++ src/modules/issues/issues.service.ts
@@ -0,0 +1,32 @@
+import { pool } from "../../db";
+import type { IIssues } from "./issues.interface";
+
+const issuesCreateIntoDB = async (payload: IIssues) => {
+
+    const { title, description, type, status, reporter_id } = payload;
+
+    const user = await pool.query(`
+        SELECT * FROM users WHERE id=$1
+        `, [reporter_id])
+
+    if (user.rows.length === 0) {
+        throw new Error("User not exists");
+    }
+
+    // const hashPassword = await bcrypt.hash(password_hash, 10);
+
+    const result = await pool.query(`
+        INSERT INTO issues(title, description, type, status, reporter_id) VALUES($1,$2,$3,COALESCE($4,'open'),$5) RETURNING *
+        `, [title, description, type, status, reporter_id])
+
+    // delete result.rows[0].password;
+
+    // console.log("service result",result);
+    return result;
+}
+
+
+export const issuesService = {
+    issuesCreateIntoDB,
+    getAlllIssuessFromDB
+}
